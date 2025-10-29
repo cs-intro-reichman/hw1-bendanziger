266e4236fd3b75356db90ab4c9b2c62079b8e00b
shai danziger <shaidanziger@MacBook-Air-sl-shai.local>
2025-10-29 19:19:20 +0200

    question1-4

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+        int currentValue = Integer.parseInt(args[0]);
+		int rate = Integer.parseInt(args[1]);
+		int n = Integer.parseInt(args[2]);
+		double futureValue = currentValue * Math.pow(1.0 + (rate / 100.0), n);
+        System.out.println("after " + n + " years, $" + currentValue + " saved at "
+		                    + rate + "% will yield $" + (int)futureValue);
+	    
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,12 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+     int theNumber = Integer.parseInt(args[0]);
+	 int ones = theNumber % 10;
+	 int tens = (theNumber/10)%10;
+	 int hundreds = theNumber / 100;
+     System.out.println(hundreds + " hundreds, " + tens + " tens, and "
+		                                        + ones + " ones.");
+	    
 	}
 }
